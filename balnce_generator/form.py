d6441bf1d456df7704d38f8d27e3c4cac3165244
Porfirio Berrocal <porfirio.berrocal@naica.mx>
2020-11-17 12:55:07 -0600

    Changes were made in __init__.py in order to improve the routes

--- balnce_generator/form.py
+++ balnce_generator/form.py
@@ -2,18 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, validators, FloatField
 from wtforms.validators import DataRequired, length
 from wtforms import DateField
+from datetime import date
 
 
 class RegistrationForm(FlaskForm):
-    dateofissue = DateField('Fecha de emisión:', default= date.today() format='%d-%m-%Y')
+    dateofissue = DateField('Fecha de emisión:', default=date.today(), format='%d-%m-%Y')
     name = StringField('Cliente:', validators=[DataRequired(), length(min=2, max=30)])
-    amount = FloatField('Capital inicial:', validators= [DataRequired(),length(min=1)])
-    product= SelectField('Producto:', choices=['Portafolio','Moneybox'])
+    amount = FloatField('Capital inicial:', validators=[DataRequired(), length(min=1)])
+    product = SelectField('Producto:', choices=['Portafolio', 'Moneybox'])
     typ = SelectField('Tipo:', choices=['Clásico', 'Dinámico'])
     annualrate = SelectField('Tasa anual:', choices=["10%"])
-    startdate = DateField('Fecha de inicio', default= date.today() format='%d-%m-%Y')
+    startdate = DateField('Fecha de inicio', default=date.today(), format='%d-%m-%Y')
     isr = SelectField('ISR', choices=['1.04%', '1.45%'])
     generate = SelectField('Generar', choices=['Estado de cuenta' 'Proyección'])
-    cutoffdate = DateField('Fecha de corte', default=date() format='%d-%m-%Y' )
+    cutoffdate = DateField('Fecha de corte', default=date(), format='%d-%m-%Y')
     submit = SubmitField('Calcular')
-
--- balnce_generator/routes.py
+++ balnce_generator/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from balnce_generator import app
 
+
 @app.route("/capturadedatos', methods=['GET', 'POST']")
 def capturadedatos():
     return render_template("captura.html", title="Captura de datos")
